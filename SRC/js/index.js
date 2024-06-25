d76ac199da7eb0806bf5d31f696bd74da3aeeac5
Viniciusfr09 <viniciusferrocha09@gmail.com>
2024-06-25 19:30:23 -0300

    Update index.js

--- SRC/js/index.js
+++ SRC/js/index.js
@@ -1,11 +1,27 @@
 const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById('btn-voltar');
 let cartaoAtual = 0;
-btnAvancar.addEventListener("click", function (){
-    console.log('cliquei');
-    
-    const cartoes = document.querySelectorAll('.cartao');
+const cartoes = document.querySelectorAll('.cartao');
+
+btnAvancar.addEventListener("click", function () {
+
+    if (cartaoAtual === cartoes.length - 1) return;
+
+    const cartaoselecionado = document.querySelector('.selecionado');
+    cartaoselecionado.classList.remove('selecionado')
+
     cartaoAtual++;
     cartoes[cartaoAtual].classList.add('selecionado');
 
+});
+
+btnVoltar.addEventListener("click", function () {
+    if (cartaoAtual === 0) return;
     
+    const cartaoselecionado = document.querySelector('.selecionado');
+    cartaoselecionado.classList.remove('selecionado')
+
+    cartaoAtual--;
+    cartoes[cartaoAtual].classList.add('selecionado');
+
 });
